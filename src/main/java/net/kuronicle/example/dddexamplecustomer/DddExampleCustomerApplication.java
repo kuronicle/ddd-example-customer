5ec8f936c005d840e7c807787ea393c4abe4da66
KUROIWA, Keijiro <kuronicle@gmail.com>
2023-02-09 11:32:23 +0900

    first commit.

---
+++ src/main/java/net/kuronicle/example/dddexamplecustomer/DddExampleCustomerApplication.java
@@ -0,0 +1,13 @@
+package net.kuronicle.example.dddexamplecustomer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddExampleCustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DddExampleCustomerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/kuronicle/example/dddexamplecustomer/DddExampleCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.kuronicle.example.dddexamplecustomer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddExampleCustomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
